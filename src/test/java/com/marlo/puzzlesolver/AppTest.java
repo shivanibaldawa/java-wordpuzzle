f8e72223755e453a021aa52c932f09fd6fbb7ba8
Shivani Baldawa <shivani.baldawa@marlo.com.au>
2019-11-13 01:35:17 +0000

    Code modified

--- src/test/java/com/marlo/puzzlesolver/AppTest.java
+++ src/test/java/com/marlo/puzzlesolver/AppTest.java
@@ -1,22 +1,46 @@
 package com.marlo.puzzlesolver;
 
-import static com.marlo.puzzlesolver.validation.validation;
+import static com.marlo.puzzlesolver.Validation.validation;
+import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.junit.Test;
 
 public class AppTest {
-  /** Rigorous Test :-) */
-  @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
-  }
 
   @Test
   public void validationtest() {
     String inputLetters = "vjhknhvhk";
 
     assertEquals(true, validation(inputLetters));
+  }
+
+  @Test
+  public void validationtestfalse() {
+    String inputLetters = "v2hknhvhk";
+
+    assertEquals(false, validation(inputLetters));
+  }
+
+  @Test
+  public void prepareDictionaryTest() {
+    PuzzleSolver pz = new PuzzleSolver();
+    String[] arr = {"Hi", "this", "is", "a", "test", "file"};
+    Set<String> set = new HashSet<>(Arrays.asList(arr));
+
+    assertEquals(set, pz.prepareDictionary());
+  }
+
+  @Test
+  public void findwordstest() {
+    PuzzleSolver pz = new PuzzleSolver();
+    String test = "file";
+
+    List<String> result = pz.findWords("life", "e", 4);
+    assertTrue(result.contains(test));
   }
 }
