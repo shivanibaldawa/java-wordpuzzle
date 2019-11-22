69a5586b42f9a7eb7323ee4e4f56bf78cf2b04f4
Shivani <shivani.baldawa@marlo.com.au>
2019-11-22 16:35:45 +1100

    modified code

--- src/main/java/com/marlo/puzzlesolver/App.java
+++ src/main/java/com/marlo/puzzlesolver/App.java
@@ -3,6 +3,7 @@ package com.marlo.puzzlesolver;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import lombok.extern.slf4j.Slf4j;
@@ -52,16 +53,19 @@ public class App implements Runnable {
 
     if (inputLetters.length() == 9 && inputLetters.matches("[a-zA-Z]+")) {
 
-      // List input = inputLetters.chars().mapToObj(c->(char) c).collect(Collectors.toList());
       words
           .lines()
           .filter(a -> a.contains(mandatoryLetter))
           .filter(a -> a.length() >= minimumLength)
           .filter(s -> PuzzleSolver.isValid(inputLetters, s))
           .forEach(System.out::println);
-
     } else {
       log.error("Enter 9 valid letters!!");
+    }
+    try {
+      words.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
